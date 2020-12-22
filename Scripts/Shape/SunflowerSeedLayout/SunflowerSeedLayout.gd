8420113ef2be662eed294465b0070c411dd23e20
ItsNorin <itsnorin@gmail.com>
2020-12-22 14:20:20 -0800

    Flipping order of seed layout

--- Scripts/Shape/SunflowerSeedLayout/SunflowerSeedLayout.gd
+++ Scripts/Shape/SunflowerSeedLayout/SunflowerSeedLayout.gd
@@ -14,13 +14,27 @@ class_name SunflowerSeedLayout
 
 const PHI = (sqrt(5) + 1) / 2
 
+# Order in which pieces are added to the arrangement
+# If true, nodes closer to center render on top of those on edges
+export (bool) var flipOrder = true setget setFlipOrder
+
+# "Spread" of distribution, higher alpha value results in more nodes on the border
 export (float, 0, 4) var alpha = 2 setget setAlpha
+
+
+
+func setFlipOrder(f:bool):
+	flipOrder = f
+	updateChildPositions()
+	pass
 
 func setAlpha(a:float):
 	alpha = a;
 	updateChildPositions()
 	pass
-
+	
+	
+	
 func updateChildPositions():
 	# gathers all Node2D children for arranging
 	var children = getAllArrangableChildren()
@@ -28,17 +42,20 @@ func updateChildPositions():
 	var cc:int = children.size()
 	
 	if cc > 1:
+# warning-ignore:narrowing_conversion
 		var bp:int = round(alpha * sqrt(cc))
 		for i in range(0,cc):
 			
 			var r:float = radius
 			if i < cc - bp:
-				r = radius * sqrt(i + 0.5) / sqrt(cc - (bp + 1) * 0.5)
+				r = radius * sqrt(i - 0.5) / sqrt(cc - (bp + 1) * 0.5)
 			
 			var theta:float = 2*PI*i / pow(PHI,2)
 			
 			var cP:Vector2 = r * Vector2(cos(theta), sin(theta))
-			(children[i]  as Node2D).position = cP
+			
+			var nodeIndex:int = (cc - i - 1) if flipOrder else (i)
+			(children[nodeIndex]  as Node2D).position = cP
 			
 	# if only one child, puts child at 0,0 offset
 	elif cc == 1:
